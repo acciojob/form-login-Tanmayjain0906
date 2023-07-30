5a3c280615c391f2ff8412e28f1a4b84998aff1e
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-30 15:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+    const input1 = document.getElementsByTagName("input")[0].value;
+	const input2 = document.getElementsByTagName("input")[1].value;
+	alert(`${input1} ${input2}`)
 }
